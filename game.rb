70375c66f96166179a0fdb780a9fe8779feeaf0d
Josh <jscanish@gmail.com>
2013-04-30 01:56:17 +0900

    first commit

---
+++ game.rb
@@ -0,0 +1,29 @@
+require 'sinatra'
+require 'shotgun'
+
+
+
+before do
+    @defeat = {rock: :scissors, paper: :rock, scissors: :paper}
+    @throws = [:rock, :scissors, :paper]
+end
+
+get '/' do 
+	erb :home
+end
+
+post '/home' do
+    @player_throw = params[:throw].to_sym 
+
+    if !@throws.include?(@player_throw)
+    	halt 403, "You must throw one of the following: rock, paper, or scissors"
+    end
+
+    @computer_throw = @throws.sample
+    erb :result
+end
+
+get '/results' do 
+    erb :results
+end
+
